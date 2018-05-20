1a27406262775f970feb29c20398be7e69fdaca3
Filho, Adilson A <adilson.a.filho@accenture.com>
2018-05-20 13:20:18 -0300

    Construindo um motor de busca com Apache Lucene

---
+++ lucene/src/main/java/com/andrades/lucene/Song.java
@@ -0,0 +1,52 @@
+package com.andrades.lucene;
+
+public class Song {
+
+
+		private String name;
+		private String artist;
+		private String genre;
+		private String lyrics;
+
+		//... CRIE OS GETTERS E SETTERS DA CLASSE
+		//...
+		//...
+		
+		
+		
+		
+		public String getName() {
+			return name;
+		}
+
+		public void setName(String name) {
+			this.name = name;
+		}
+
+		public String getArtist() {
+			return artist;
+		}
+
+		public void setArtist(String artist) {
+			this.artist = artist;
+		}
+
+		public String getGenre() {
+			return genre;
+		}
+
+		public void setGenre(String genre) {
+			this.genre = genre;
+		}
+
+		public String getLyrics() {
+			return lyrics;
+		}
+
+		public void setLyrics(String lyrics) {
+			this.lyrics = lyrics;
+		}
+
+	
+
+}
